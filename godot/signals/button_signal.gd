5c72f2e7cb9d0c6fe7c4022afe03361f643a132d
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2023-06-16 13:43:34 +0200

    add character, improve button signal demo visuals

--- godot/signals/button_signal.gd
+++ godot/signals/button_signal.gd
@@ -5,6 +5,6 @@ func _ready():
 	# Connect the button's pressed signal to a lambda function, using code.
 	%Button.pressed.connect(
 		func toggle_texture_and_button():
-			%TextureRect.visible = not %TextureRect.visible
-			%Button.text = "Hide" if %TextureRect.visible else "Show"
+			%Character.visible = not %Character.visible
+			%Button.text = "Hide" if %Character.visible else "Show"
 	)
