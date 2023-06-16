93b94751353ce02d621cd5d4f6e0a86889392a7f
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-06-16 18:10:53 +0300

    chore: rmeove outdated godot_example project

--- godot_example/godot_migration_3/gdscript_keywords/gdscript_keywords.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export var velocity := 100.0
-onready var player: Sprite = $Player
-
-
-func _process(delta: float) -> void:
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	player.position += input_direction * velocity * delta
