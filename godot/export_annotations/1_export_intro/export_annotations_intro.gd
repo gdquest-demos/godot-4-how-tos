edcfc89410f2f41dbedb92c459b027494a2311fb
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-06-17 14:33:19 +0300

    improvement(export_annotations): replace patterns

--- godot/export_annotations/1_export_intro/export_annotations_intro.gd
+++ godot/export_annotations/1_export_intro/export_annotations_intro.gd
@@ -1,26 +1,21 @@
 extends Node2D
 
-enum Type {BONE, ASYNC, ASYNC_FUNCTION}
+enum Type {ELLIPSE, RECTANGLE}
 
 const PATTERNS := {
-	Type.BONE: {
-		pattern_sampler = preload("res://shared/background/bone_pattern.png"),
+	Type.ELLIPSE: {
+		pattern_sampler = preload("../ellipses.svg"),
 		bg_color = Color("061d54"),
 		pattern_color = Color("007ac3"),
 	},
-	Type.ASYNC: {
-		pattern_sampler = preload("res://shared/background/async_pattern.svg"),
-		bg_color = Color("6a1900"),
-		pattern_color = Color("b05737"),
-	},
-	Type.ASYNC_FUNCTION: {
-		pattern_sampler = preload("res://shared/background/async_function_pattern.svg"),
+	Type.RECTANGLE: {
+		pattern_sampler = preload("../rectangles.svg"),
 		bg_color = Color("15593f"),
 		pattern_color = Color("9fdd51"),
 	},
 }
 
-@export var type := Type.BONE
+@export var type := Type.ELLIPSE
 
 
 func _ready() -> void:
